ff4ff6bff09a28773abbd2f8959237c376eb5628
Anisha100 <87559560+Anisha100@users.noreply.github.com>
2022-06-23 12:23:07 +0530

    Update index.jsx

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -43,6 +43,7 @@ export const Contact = () => {
                         </div>
                         <div className="w-full max-w-xl xl:px-8 xl:w-5/12">
                             <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
+                                
                                 <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
                                     Leave a Message
                                 </h3>
@@ -55,7 +56,7 @@ export const Contact = () => {
                                             First name
                                         </label>
                                         <input
-                                            placeholder="John"
+                                            placeholder=""
                                             required
                                             type="text"
                                             className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
@@ -71,7 +72,7 @@ export const Contact = () => {
                                             Last name
                                         </label>
                                         <input
-                                            placeholder="Doe"
+                                            placeholder=""
                                             required
                                             type="text"
                                             className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
@@ -87,12 +88,13 @@ export const Contact = () => {
                                             E-mail
                                         </label>
                                         <input
-                                            placeholder="john.doe@example.org"
+                                            placeholder=""
                                             required
                                             type="text"
                                             className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
                                             id="email"
                                             name="email"
+                                            
                                         />
                                     </div>
                                     <div className="mb-1 sm:mb-2">
@@ -112,15 +114,15 @@ export const Contact = () => {
                                         />
                                     </div>
                                     <div className="mt-4 mb-2 sm:mb-4">
-                                        <button
+                                        <button onclick="sendMail()"
                                             type="submit"
                                             className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500  py-2  focus:ring transform  hover:scale-105  ease-in-out focus:shadow-outline focus:outline-none"
                                         >
-                                            Subscribe
+                                            Submit
                                         </button>
                                     </div>
                                     <p className="text-xs text-gray-600 sm:text-sm">
-                                        We respect your privacy. Unsubscribe at any time.
+                                    We will get back to you!
                                     </p>
                                 </form>
                             </div>
@@ -130,4 +132,14 @@ export const Contact = () => {
             </div>
         </div>
     );
+  
+      
 };
+const sendMail = () => {
+    const mailAdress = "someone@example.com"
+    const ccRecipients =  "mrinal.annand@gmail.com"
+    const subject = encodeURIComponent("This is my subject")
+    const body = encodeURIComponent(document.getElementById('myText').value)
+    const  link= `mailto:${mailAdress}?cc=${ccRecipients}&subject=${subject}&body=${body}`
+    window.location.href = link;
+  }
